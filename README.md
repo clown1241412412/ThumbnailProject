e8c52193d96ff4c300d55f7502cfd481141b11a3
clown1241412412 <1013ff@gmail.com>
2025-07-30 18:53:58 +0900

    add: 기초 파일 추가

---
+++ README.md
@@ -0,0 +1,7 @@
+# Thumbnail 추출 프로젝트
+
+실행 방법
+
+```
+python main.py
+```
---
+++ main.py
@@ -0,0 +1,12 @@
+import requests
+from datetime import datetime
+
+video_url = input("링크를 붙여넣어주세요.(Ctrl + Shift + V)\n")
+video_id = video_url.split("v=")[-1]
+thumbnail_url = f"https://img.youtube.com/vi/{video_id}/0.jpg"
+response = requests.get(thumbnail_url)
+
+now = datetime.now().strftime("%Y%m%d_%H%M%S")  # 예: 20250730_152345
+with open(f"thumbnail{now}.jpg", "wb") as f:
+   f.write(response.content)
+print(" 썸네일 다운로드 완료!")
